bc9691a37091b8b7dfad2fcc55980ef7eb6189f9
Michael Friis <friism@gmail.com>
2012-05-23 18:15:46 -0700

    also use mysql-uri

--- MySQLExample/Global.asax.cs
+++ MySQLExample/Global.asax.cs
@@ -61,7 +61,7 @@ namespace MySQLExample
 
 		protected ISessionFactory CreateSessionFactory()
 		{
-			var uriString = ConfigurationManager.AppSettings["XEROUND_DATABASE_INTERNAL_URL"];
+			var uriString = ConfigurationManager.AppSettings["XEROUND_DATABASE_INTERNAL_URL"] ?? ConfigurationManager.AppSettings["MYSQL_URI"];
 			var uri = new Uri(uriString);
 			var connectionString = string.Format("Server={0};Port={1};Database={2};User Id={3};Password={4};",
 				uri.Host, uri.Port, uri.AbsolutePath.Trim('/'), uri.UserInfo.Split(':').First(),
